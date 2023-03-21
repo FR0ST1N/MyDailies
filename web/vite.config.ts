f7e4c4fd5038a7622e32bcdfcf0310ba5d49de3e
FR0ST1N <iamfrostin@gmail.com>
2023-03-21 18:28:24 +0530

    build(web): add eslint to vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
     proxy: {
       '/api': {
